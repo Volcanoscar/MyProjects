6822bdecb679bdb4021099b6561dd277b97809c1
limxing <474631895@qq.com>
2015-05-22 23:51:53 +0800

    增加了控件抖动的效果，手机震动的效果

--- 手机安全卫士/src/com/limxing/safe/activity/ToolLocationActivity.java
+++ 手机安全卫士/src/com/limxing/safe/activity/ToolLocationActivity.java
@@ -1,10 +1,16 @@
 package com.limxing.safe.activity;
 
 import android.app.Activity;
+import android.app.Service;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,12 +19,17 @@ import com.limxing.safe.R;
 import com.limxing.safe.db.dao.NumberAddressDao;
 import com.limxing.safe.utils.ToastUtils;
 
+/*
+ * 作者：Limxing 时间： 2015-5-22 下午11:05:28
+ * 
+ * 描述：这是归属地查询的Activity类
+ */
 public class ToolLocationActivity extends Activity {
 	private EditText tool_location_et;
 	private Button tool_location_bt;
 	private TextView tool_location_tv;
 	private String mNumber;
-	private NumberAddressDao mNumberAddressDao;
+	private Vibrator vibrator;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +39,64 @@ public class ToolLocationActivity extends Activity {
 	}
 
 	private void init() {
-		mNumberAddressDao=new NumberAddressDao();
+		// 文本框的实时查询
+		tool_location_et.addTextChangedListener(new TextWatcher() {
+
+			private String location;
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				String number = s.toString();
+				location = NumberAddressDao.findNumber(number);
+				tool_location_tv.setText("号码归属地为：" + location);
+			}
+		});
+		// 按钮的点击事件
 		tool_location_bt.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				mNumber=tool_location_et.getText().toString().trim();
-				if(TextUtils.isEmpty(mNumber)){
+				mNumber = tool_location_et.getText().toString().trim();
+				if (TextUtils.isEmpty(mNumber)) {
+					//增加控件抖动效果
+					Animation shake = AnimationUtils.loadAnimation(
+							ToolLocationActivity.this, R.anim.shake);
+					tool_location_et.startAnimation(shake);
+					//增加手机震动效果
+					vibrator = (Vibrator) getApplication().getSystemService(
+							Service.VIBRATOR_SERVICE);
+					vibrator.vibrate(200);
 					ToastUtils.showToast(ToolLocationActivity.this, "请输入号码");
 					return;
 				}
-				String location=mNumberAddressDao.findNumber(mNumber);
-				tool_location_tv.setText("号码归属地为："+location);
-				
+				String location = NumberAddressDao.findNumber(mNumber);
+				tool_location_tv.setText("号码归属地为：" + location);
+
 			}
 		});
-		
-		
+
 	}
 
 	private void initUI() {
 		setContentView(R.layout.tools_location);
 		tool_location_et = (EditText) findViewById(R.id.tools_location_et);
 		tool_location_bt = (Button) findViewById(R.id.tool_location_bt);
-		tool_location_tv=(TextView) findViewById(R.id.tool_location_tv);
-	
+		tool_location_tv = (TextView) findViewById(R.id.tool_location_tv);
+
 	}
 
 	// 返回键
--- 手机安全卫士/src/com/limxing/safe/db/dao/NumberAddressDao.java
+++ 手机安全卫士/src/com/limxing/safe/db/dao/NumberAddressDao.java
@@ -6,13 +6,13 @@ import android.database.sqlite.SQLiteDatabase;
 /*
  * 作者：Limxing 时间： 2015-5-22 下午10:44:20
  * 
- * 描述：操作归属地数据库的类
+ * 描述：操作归属地数据库的类，很烂，db都不关闭
  */
 public class NumberAddressDao {
-	private String location;
-	private SQLiteDatabase db;
+	private static String location;
+	private static SQLiteDatabase db;
 
-	public String findNumber(String mNumber) {
+	public static String findNumber(String mNumber) {
 		db = SQLiteDatabase.openDatabase(
 				"/data/data/com.limxing.safe/files/address.db", null,
 				SQLiteDatabase.OPEN_READONLY);
@@ -23,19 +23,22 @@ public class NumberAddressDao {
 			String number = mNumber.substring(0, 7);
 			return findLocation(number);
 		case 7:
+			db.close();
 			return "本地号码";
 		case 8:
+			db.close();
 			return "本地号码";
 		case 10:
 			String number1 = mNumber.substring(0, 3);
 			return findLocation(number1);
 
 		}
+		db.close();
 		return "查不到的号码";
 
 	}
 
-	public String findLocation(String number) {
+	public static String findLocation(String number) {
 		Cursor cursor = db
 				.rawQuery(
 						"select location from data2 where id=(select outkey from data1 where id=?)",

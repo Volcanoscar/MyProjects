21c65f52822fc3001d06776c45df90f5718cdb23
limxing <474631895@qq.com>
2015-05-25 19:58:51 +0800

    增加短信备份的功能

--- 手机安全卫士/src/com/limxing/safe/activity/ToolsActivity.java
+++ 手机安全卫士/src/com/limxing/safe/activity/ToolsActivity.java
@@ -1,6 +1,9 @@
 package com.limxing.safe.activity;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,9 +11,15 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.limxing.safe.R;
+import com.limxing.safe.utils.SmsBackUp;
+import com.limxing.safe.utils.ToastUtils;
+import com.limxing.safe.utils.SmsBackUp.BackState;
 
 public class ToolsActivity extends Activity {
 	private TextView tools_tv_location;
+	private TextView tools_tv_smsBackup;
+	private TextView tools_tv_smsRestor;
+	private ProgressDialog pd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +31,13 @@ public class ToolsActivity extends Activity {
 	private void initUI() {
 		setContentView(R.layout.tools_main);
 		tools_tv_location = (TextView) findViewById(R.id.tools_tv_location);
+		tools_tv_smsBackup = (TextView) findViewById(R.id.tools_tv_smsBackup);
+		tools_tv_smsRestor = (TextView) findViewById(R.id.tools_tv_smsRestor);
 
 	}
 
 	private void init() {
+		// 查询归属地点击事件
 		tools_tv_location.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -33,6 +45,59 @@ public class ToolsActivity extends Activity {
 				Intent intent = new Intent(ToolsActivity.this,
 						ToolLocationActivity.class);
 				startActivity(intent);
+			}
+		});
+		// 备份短信点击事件
+		tools_tv_smsBackup.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				pd = new ProgressDialog(ToolsActivity.this);
+				pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+				pd.setMessage("正在备份");
+				pd.show();
+				new Thread() {
+					public void run() {
+
+						try {
+							SmsBackUp.backUp(getApplicationContext(),
+									new BackState() {
+
+										@Override
+										public void process(int process) {
+											pd.setProgress(process);
+
+										}
+
+										@Override
+										public void beforeBackup(int size) {
+											pd.setMax(size);
+										}
+									});
+							ToastUtils.showToast(ToolsActivity.this, "备份完成");
+						} catch (IllegalArgumentException e) {
+							ToastUtils.showToast(ToolsActivity.this, "备份失败");
+							e.printStackTrace();
+						} catch (IllegalStateException e) {
+							ToastUtils.showToast(ToolsActivity.this,
+									e.getMessage());
+						} catch (IOException e) {
+							ToastUtils.showToast(ToolsActivity.this, "备份失败");
+							e.printStackTrace();
+						} finally {
+							pd.dismiss();
+						}
+
+					}
+				}.start();
+			}
+		});
+		// 还原短信点击事件
+		tools_tv_smsRestor.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
 			}
 		});
 	}
---
+++ 手机安全卫士/src/com/limxing/safe/utils/SmsBackUp.java
@@ -0,0 +1,110 @@
+package com.limxing.safe.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.util.Xml;
+
+/**
+ * 
+ * 版权：李利锋个人版权所有
+ * 
+ * 版本：1.0
+ * 
+ * 作者：李利锋
+ * 
+ * 创建日期：2015-5-25 下午6:20:01
+ * 
+ * 描述：短信备份还原的工具类
+ * 
+ * 
+ * 修订历史：
+ * 
+ */
+public class SmsBackUp {
+
+	public static boolean backUp(final Context context, BackState backState)
+			throws IllegalArgumentException, IllegalStateException, IOException {
+
+		ContentResolver resolver = context.getContentResolver();
+		Uri uri = Uri.parse("content://sms/");
+		Cursor cursor = resolver.query(uri, new String[] { "address", "body",
+				"type", "date" }, null, null, null);
+		XmlSerializer serializer = Xml.newSerializer();
+		File sdDir = Environment.getExternalStorageDirectory();
+		long free = sdDir.getFreeSpace();
+		// 判断是否SD卡存在以及空间是否足够
+		if (Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)
+				&& free > 1024l * 1024l) {
+			// 创建保存位置
+			File file = new File(sdDir, "smsBackup.xml");
+			FileOutputStream os = new FileOutputStream(file);
+			// 初始化xml文件的序列化器
+			serializer.setOutput(os, "utf-8");
+			// 写xml文件的开头
+			serializer.startDocument("utf-8", true);
+			// 写根节点
+			int size = cursor.getCount();
+			backState.beforeBackup(size);
+			serializer.startTag(null, "smss");
+			serializer.attribute(null, "size", String.valueOf(size));
+			// 初始化进度条
+			int process = 0;
+			while (cursor.moveToNext()) {
+				serializer.startTag(null, "sms");
+				serializer.startTag(null, "body");
+				try {
+					// 加密处理
+					serializer.text(Crypto.encrypt("123", cursor.getString(1)));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				serializer.endTag(null, "body");
+				serializer.startTag(null, "address");
+				serializer.text(cursor.getString(0));
+				serializer.endTag(null, "address");
+				serializer.startTag(null, "type");
+				serializer.text(cursor.getString(2));
+				serializer.endTag(null, "type");
+				serializer.startTag(null, "date");
+				serializer.text(cursor.getString(3));
+				serializer.endTag(null, "date");
+				serializer.endTag(null, "sms");
+
+				process++;
+				backState.process(process);
+				SystemClock.sleep(500);
+
+			}
+			cursor.close();
+			serializer.endTag(null, "smss");
+			serializer.endDocument();
+			os.flush();
+			os.close();
+			return true;
+		} else {
+			throw new IllegalStateException("SD卡不存在或者空间不足");
+		}
+
+	}
+
+	public interface BackState {
+		// 备份时
+		public void process(int process);
+
+		// 备份前
+		public void beforeBackup(int size);
+	}
+
+}

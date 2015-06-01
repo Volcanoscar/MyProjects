7c8732d02d4eb1b3c363aecd1d8f540b2fe7b155
limxing <474631895@qq.com>
2015-06-01 21:17:04 +0800

    添加杀毒的检查操作

--- 手机安全卫士/src/com/limxing/safe/activity/AntivirusActivity.java
+++ 手机安全卫士/src/com/limxing/safe/activity/AntivirusActivity.java
@@ -1,43 +1,156 @@
 package com.limxing.safe.activity;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.limxing.safe.R;
+import com.limxing.safe.db.dao.AntivirusDao;
+import com.limxing.safe.utils.Md5Utils;
+
 /*
  * 作者：Limxing 时间： 2015-5-30 下午7:24:23
  * 
  * 描述：病毒查杀的模块
  */
 public class AntivirusActivity extends Activity {
+	protected static final int SCAN_START = 1;
+	protected static final int SCANING = 2;
+	protected static final int END = 3;
 	private ImageView antivirus_main_iv;
 	private TextView antivirus_main_tv;
 	private ProgressBar antivirus_main_pb;
 	private LinearLayout antivirus_main_ll;
+	private PackageManager pm;
+	private boolean flag;
+	private Handler handler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case SCAN_START:
+				antivirus_main_tv.setText("初始化病毒扫描引擎");
+				break;
+
+			case SCANING:
+				ScanInfo scanInfo = (ScanInfo) msg.obj;
+				antivirus_main_tv.setText("正在扫描：" + scanInfo.appname);
+				TextView view = new TextView(getApplicationContext());
+				if (scanInfo.isvirtus) {
+					view.setTextColor(Color.RED);
+				} else {
+					view.setTextColor(Color.BLACK);
+				}
+				view.setText(scanInfo.appname + ":" + scanInfo.desc);
+				// 把新建的文本文件放进ListView控件中，0代表放置的位置
+				antivirus_main_ll.addView(view, 0);
+
+				break;
+			case END:
+				antivirus_main_tv.setText("扫描完成！");
+				antivirus_main_iv.clearAnimation();
+				Toast.makeText(AntivirusActivity.this, "扫描完成", 0).show();
+				break;
+			}
+		};
+
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.antivirus_main);
-		antivirus_main_iv=(ImageView) findViewById(R.id.antivirus_main_iv);
-		antivirus_main_tv=(TextView) findViewById(R.id.antivirus_main_tv);
-		antivirus_main_pb=(ProgressBar) findViewById(R.id.antivirus_main_pb);
-		antivirus_main_ll=(LinearLayout) findViewById(R.id.antivirus_main_ll);
-		RotateAnimation ra=new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		pm = getPackageManager();
+		antivirus_main_iv = (ImageView) findViewById(R.id.antivirus_main_iv);
+		antivirus_main_tv = (TextView) findViewById(R.id.antivirus_main_tv);
+		antivirus_main_pb = (ProgressBar) findViewById(R.id.antivirus_main_pb);
+		antivirus_main_ll = (LinearLayout) findViewById(R.id.antivirus_main_ll);
+		RotateAnimation ra = new RotateAnimation(0, 360,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
 		ra.setDuration(1000);
 		ra.setRepeatCount(Animation.INFINITE);
 		antivirus_main_iv.startAnimation(ra);
-		
-		
-		
-		
-		
+		scanVirus();
+
+	}
+
+	// 扫描病毒
+	private void scanVirus() {
+		flag = true;
+		new Thread() {
+			public void run() {
+				Message msg = Message.obtain();
+				msg.what = SCAN_START;
+				handler.sendMessage(msg);
+
+				int process = 0;
+				// 获取包集合
+				List<PackageInfo> packageInfos = pm.getInstalledPackages(0);
+				int max = packageInfos.size();
+				antivirus_main_pb.setMax(max);
+				for (PackageInfo info : packageInfos) {
+					if (!flag) {
+						return;
+					}
+					String path = info.applicationInfo.sourceDir;
+					// 工具类获取这个文件的特征码
+					String md5Info = Md5Utils.getFileMd5(path);
+					String result = AntivirusDao.find(md5Info);
+					ScanInfo scanInfo = new ScanInfo();
+					if (result == null) {
+						scanInfo.isvirtus = false;
+						scanInfo.desc = "扫描通过";
+					} else {
+						scanInfo.isvirtus = true;
+						scanInfo.desc = result;
+					}
+					scanInfo.packname = info.packageName;
+					scanInfo.appname = info.applicationInfo.loadLabel(pm)
+							.toString();
+					// 发送更新UI通知
+					msg = Message.obtain();
+					msg.obj = scanInfo;
+					msg.what = SCANING;
+					handler.sendMessage(msg);
+					process++;
+					antivirus_main_pb.setProgress(process);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				msg = Message.obtain();
+				msg.what = END;
+				handler.sendMessage(msg);
+			}
+		}.start();
+	}
+
+	public class ScanInfo {
+		String appname;
+		boolean isvirtus;
+		String desc;
+		String packname;
+	}
+
+	@Override
+	protected void onDestroy() {
+		flag = false;
+		super.onDestroy();
 	}
 
 }
--- 手机安全卫士/src/com/limxing/safe/db/dao/AntivirusDao.java
+++ 手机安全卫士/src/com/limxing/safe/db/dao/AntivirusDao.java
@@ -1,17 +1,103 @@
 package com.limxing.safe.db.dao;
 
+import java.io.File;
+
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+/*
+ * 作者：Limxing 时间： 2015-5-31 下午7:47:30
+ * 
+ * 描述：病毒数据库操作类
+ */
 public class AntivirusDao {
 	private static SQLiteDatabase db;
 
-	public static String find(String packname) {
+	/**
+	 * 数据库的查找功能
+	 * 
+	 * @param md5
+	 * @return 描述或者null
+	 */
+	public static String find(String md5) {
+		String desc = null;
 		db = SQLiteDatabase.openDatabase(
 				"/data/data/com.limxing.safe/files/antivirus.db", null,
 				SQLiteDatabase.OPEN_READONLY);
-		db.query("datable", new String[] { "desc" }, "md5=?", new String[] {},
-				null, null, null);
-		return null;
+		Cursor cursor = db.rawQuery("select desc from datable where md5=?",
+				new String[] { md5 });
+		if (cursor.moveToNext()) {
+			desc = cursor.getString(0);
+		}
+		cursor.close();
+		db.close();
+		return desc;
 	}
 
+	/**
+	 * 判断数据库属否存在
+	 * 
+	 * @return true存在 false不存在
+	 */
+	public static boolean isExist() {
+		File file = new File("/data/data/com.limxing.safe/files/antivirtus.db");
+		return file.exists() && file.length() > 0;
+
+	}
+
+	/**
+	 * 获取数据可以的版本号
+	 * 
+	 * @return 数字字符串
+	 */
+	public static String getDBVersion() {
+		SQLiteDatabase db = SQLiteDatabase.openDatabase(
+				"/data/data/com.limxing.safe/files/antivirtus.db", null,
+				SQLiteDatabase.OPEN_READONLY);
+		String version = "0";
+		Cursor cursor = db.rawQuery("select subcnt from version", null);
+		if (cursor.moveToNext()) {
+			version = cursor.getString(0);
+		}
+		cursor.close();
+		db.close();
+
+		return version;
+	}
+
+	/**
+	 * 更新数据库版本的操作
+	 * 
+	 * @param newVersion
+	 */
+	public static void updataDBVersion(int newVersion) {
+		SQLiteDatabase db = SQLiteDatabase.openDatabase(
+				"/data/data/com.limxing.safe/files/antivirtus.db", null,
+				SQLiteDatabase.OPEN_READWRITE);
+		ContentValues values = new ContentValues();
+		values.put("subcnt", newVersion);
+		db.update("version", values, null, null);
+		db.close();
+	}
+
+	/**
+	 * 更新数据库的操作
+	 * 
+	 * @param desc
+	 * @param md5
+	 */
+	public static void add(String desc, String md5) {
+		SQLiteDatabase db = SQLiteDatabase.openDatabase(
+				"/data/data/com.limxing.safe/files/antivirtus.db", null,
+				SQLiteDatabase.OPEN_READWRITE);
+		ContentValues values = new ContentValues();
+		values.put("md5", md5);
+		values.put("desc", desc);
+		values.put("type", 6);
+		values.put("name", "Android.Hack.i22hkt.a");
+		db.insert("datable", null, values);
+		db.close();
+
+	}
 }
